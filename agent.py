cf24054a8e55f08204635d718d25e98c7439f981
Haakam Aujla <haakamsingh21@gmail.com>
2025-07-19 17:04:28 -0700

    AgentMail websocket client

--- agent.py
+++ agent.py
@@ -1,13 +1,13 @@
 import os
 import logging
-import json
 import asyncio
 
 from dotenv import load_dotenv
 
-from agentmail import AgentMail
+from agentmail import AgentMail, AsyncAgentMail
+from agentmail.websockets.types import Subscribe, MessageReceived
 from agentmail_toolkit.livekit import AgentMailToolkit
-from websockets.asyncio.client import connect
+
 
 from livekit import agents
 from livekit.agents import AgentSession, Agent, RoomInputOptions, JobContext
@@ -19,62 +19,64 @@ from livekit.plugins import (
 load_dotenv()
 
 api_key = os.getenv("AGENTMAIL_API_KEY")
-ws_url = f"wss://ws.agentmail.to/v0?auth_token={api_key}"
-
 
 logger = logging.getLogger(__name__)
 
-client = AgentMail()
-
-inbox = client.inboxes.create(
-    username="livekit",
-    display_name="LiveKit",
-    client_id="livekit-inbox",
-)
-
 
 class Assistant(Agent):
+    inbox_id: str
     ws_task: asyncio.Task | None = None
 
     def __init__(self) -> None:
+        inbox = AgentMail().inboxes.create(
+            username="livekit",
+            display_name="LiveKit",
+            client_id="livekit-inbox",
+        )
+
+        self.inbox_id = inbox.inbox_id
+
         super().__init__(
             instructions=f"""
-            You are a helpful voice and email AI assistant. You can send, receive, and reply to emails. Your email address is {inbox.inbox_id}.
-            IMPORTANT: When using email tools, use "{inbox.inbox_id}" as the inbox_id parameter. When writing an email, refer to yourself as "LiveKit" in the signature. Always speak in English.
+            You are a helpful voice and email AI assistant. You can send, receive, and reply to emails. Your email address is {self.inbox_id}.
+            IMPORTANT: When using email tools, use "{self.inbox_id}" as the inbox_id parameter. When writing an email, refer to yourself as "LiveKit" in the signature. Always speak in English.
             """,
-            tools=AgentMailToolkit(client=client).get_tools(
+            tools=AgentMailToolkit().get_tools(
                 ["list_threads", "get_thread", "send_message", "reply_to_message"]
             ),
         )
 
     async def _websocket_task(self):
         try:
-            async with connect(ws_url) as ws:
-                logger.info("Connected to AgentMail websocket server")
+            async with AsyncAgentMail().websockets.connect(
+                auth_token=api_key
+            ) as socket:
+                logger.info("Connected to AgentMail websocket")
 
-                await ws.send(
-                    json.dumps({"type": "subscribe", "inbox_ids": [inbox.inbox_id]})
-                )
+                await socket.send_subscribe(Subscribe(inbox_ids=[self.inbox_id]))
 
                 while True:
-                    data = json.loads(await ws.recv())
+                    data = await socket.recv()
                     logger.debug("Received data: %s", data)
 
-                    if (
-                        data["type"] == "event"
-                        and data["event_type"] == "message.received"
-                    ):
+                    if isinstance(data, MessageReceived):
                         self.session.interrupt()
 
                         await self.session.generate_reply(
-                            instructions=f""""Say "I've recieved an email" and then read the email.""",
-                            user_input=str(data["message"]),
+                            instructions=f"""Say "I've recieved an email" and then read the email.""",
+                            user_input=data.message.model_dump_json(),
                         )
+        except Exception as e:
+            logger.error("Error in websocket task: %s", e)
         finally:
-            logger.info("Disconnected from AgentMail websocket server")
+            logger.info("Disconnected from AgentMail websocket")
 
     async def on_enter(self):
         self.ws_task = asyncio.create_task(self._websocket_task())
+
+        await self.session.generate_reply(
+            instructions=f"In English, greet the user, inform them that you can recieve emails at {self.inbox_id}, and offer your assistance."
+        )
 
     async def on_exit(self):
         if self.ws_task:
@@ -93,10 +95,6 @@ async def entrypoint(ctx: JobContext):
     )
 
     await ctx.connect()
-
-    await session.generate_reply(
-        instructions=f"In English, greet the user, inform them that you can recieve emails at {inbox.inbox_id}, and offer your assistance."
-    )
 
 
 if __name__ == "__main__":
